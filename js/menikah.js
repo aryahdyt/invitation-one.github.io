c71edbeec6809b67e30ada3baaf33f0b48383e86
arya <aryaa.hdyt@gmail.com>
2022-09-06 21:55:56 +0700

    click menu to section

--- js/menikah.js
+++ js/menikah.js
@@ -172,6 +172,14 @@ function scrollFunction() {
   }
 }
 
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
+
 $.fn.serializeObject = function()
 {
     var o = {};
